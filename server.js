58ebaf22d323e816709a514374bf372442716044
Aishjahan <aishjahankhan7376@gmail.com>
2023-12-18 12:11:30 +0530

    added folders

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 //Testing file
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+
+
+// -------------------------------------------------
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
+});
