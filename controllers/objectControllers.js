d689640ebbccb436f90be433e169abb5e384dfce
Yashs Tiwari <yashstiwari03@gmail.com>
2023-12-18 15:40:16 +0530

    setup

---
+++ controllers/objectControllers.js
@@ -0,0 +1,21 @@
+const asyncHandler = require("express-async-handler");
+const { constants } = require("../helper/constants");
+
+//@desc Register New Guide
+//@route POST /api/guide
+//@access
+const fetchDescription = asyncHandler(async (req, res) => {
+  try {
+    //error handling for empty fields
+    const { id } = req.body;
+    if (!id) {
+      res.status(constants.VALIDATION_ERROR);
+      throw new Error("Invalid QR code");
+    }
+    const newObject = await Objects.create(req.body);
+    res.status(constants.CREATED).json(newObject);
+  } catch (err) {
+    res.status(constants.SERVER_ERROR);
+    throw new Error(err.message);
+  }
+});
---
+++ routes/objectRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { fetchDescription } = require("../controllers/objectControllers");
+
+router.route("/:id").post(fetchDescription);
+
+module.exports = router;
